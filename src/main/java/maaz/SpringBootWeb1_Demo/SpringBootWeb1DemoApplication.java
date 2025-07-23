b76807fcb3d897305175044b86057ca10f2fb723
maaztausif <maaztausif48@gmail.com>
2025-07-23 17:23:59 +0200

    Project created

---
+++ src/main/java/maaz/SpringBootWeb1_Demo/SpringBootWeb1DemoApplication.java
@@ -0,0 +1,13 @@
+package maaz.SpringBootWeb1_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeb1DemoApplication.class, args);
+	}
+
+}
