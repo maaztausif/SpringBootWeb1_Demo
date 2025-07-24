4c11df783177619dd6cb0bad9e3eadbcb5a2c1f9
maaztausif <maaztausif48@gmail.com>
2025-07-24 04:42:43 +0200

    model attribute added 100% complete
    -- refactor the code using model attribi=ute

--- src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
+++ src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,17 +19,39 @@ public class HomeController {
         System.out.println("Home is open!");
         return  "index";
     }
-    @RequestMapping("add")
-    public ModelAndView add(@RequestParam("num1") int numTest, int num2, ModelAndView mv){
-//        int num1 = Integer.parseInt(req.getParameter("num1"));
-//        int num2 = Integer.parseInt(req.getParameter("num2"));
-        int result = numTest+num2;
 
-       mv.addObject("result",result);
-       mv.setViewName("result");
-        System.out.println("add result! "+ result);
-        return  mv;
+    @RequestMapping("addAlien")
+    public String addAlien(@ModelAttribute("alien1") Alien alien){
+
+        return  "result";
     }
+
+    @ModelAttribute("course")
+    public String courseName(){
+        return "java";
+    }
+
+//    @RequestMapping("addAlien")
+//    public ModelAndView addAlien(@RequestParam("aid") int numTest, String aname, ModelAndView mv){
+//
+//        Alien alien = new Alien();
+//
+//        alien.setAid(numTest);
+//        alien.setAname(aname);
+//
+//        mv.addObject("alien",alien);
+//        mv.setViewName("result");
+//        return  mv;
+//    }
+
+
+//    @RequestMapping("add")
+//    public ModelAndView add(@RequestParam("num1") int numTest, int num2, ModelAndView mv){
+//        int result = numTest+num2;
+//        mv.addObject("result",result);
+//        mv.setViewName("result");
+//        return  mv;
+//    }
 //    @RequestMapping("add")
 //    public String add(HttpServletRequest req, HttpSession session){
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
--- src/main/webapp/views/result.jsp
+++ src/main/webapp/views/result.jsp
@@ -5,8 +5,11 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     </body>
-        <h2>Result Is =  <%= session.getAttribute("result")  %>  </h2>
+        <h2> Welcome To Maaz App </h2>
 
-        <h2>Result Is =  ${result}  </h2>
+        <p>Result Is =  ${alien1}  <p>
+
+
+        <p>  Welcome to the ${course} world  <p>
     </body>
 </html>
