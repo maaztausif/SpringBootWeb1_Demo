3b373108b83cb9238b8c966272efaa4cdf03d6d4
maaztausif <maaztausif48@gmail.com>
2025-07-24 03:29:36 +0200

    Add Param
    -- Remove Http req and add param

--- src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
+++ src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,14 +15,25 @@ public class HomeController {
         return  "index.jsp";
     }
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
-        int result = num1+num2;
+    public String add(@RequestParam("num1") int numTest, int num2, HttpSession session){
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = numTest+num2;
 
         session.setAttribute("result",result);
 
         System.out.println("add result! "+ result);
         return  "result.jsp";
     }
+//    @RequestMapping("add")
+//    public String add(HttpServletRequest req, HttpSession session){
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
+//        int result = num1+num2;
+//
+//        session.setAttribute("result",result);
+//
+//        System.out.println("add result! "+ result);
+//        return  "result.jsp";
+//    }
 }
