5a77e1801dbd46b25b3bbf9cfbd6bae1f51c41a2
maaztausif <maaztausif48@gmail.com>
2025-07-24 03:40:09 +0200

    Add ModelAndView
    -- Sending data using ModelAndView

--- src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
+++ src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.rmi.MarshalledObject;
 
@@ -18,14 +19,15 @@ public class HomeController {
         return  "index";
     }
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int numTest, int num2, Model model){
+    public ModelAndView add(@RequestParam("num1") int numTest, int num2, ModelAndView mv){
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
 //        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = numTest+num2;
 
-        model.addAttribute("result",result);
+       mv.addObject("result",result);
+       mv.setViewName("result");
         System.out.println("add result! "+ result);
-        return  "result";
+        return  mv;
     }
 //    @RequestMapping("add")
 //    public String add(HttpServletRequest req, HttpSession session){
