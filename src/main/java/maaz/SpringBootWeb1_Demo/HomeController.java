b62000b87fa585df2ab5fe249e4c482f9f93ba61
maaztausif <maaztausif48@gmail.com>
2025-07-24 03:37:10 +0200

    adding Model
    -- Sending data using model
    -- Use suffix and prefix

--- src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
+++ src/main/java/maaz/SpringBootWeb1_Demo/HomeController.java
@@ -3,8 +3,11 @@ package maaz.SpringBootWeb1_Demo;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.rmi.MarshalledObject;
 
 @Controller
 public class HomeController {
@@ -12,18 +15,17 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("Home is open!");
-        return  "index.jsp";
+        return  "index";
     }
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int numTest, int num2, HttpSession session){
+    public String add(@RequestParam("num1") int numTest, int num2, Model model){
 //        int num1 = Integer.parseInt(req.getParameter("num1"));
 //        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = numTest+num2;
 
-        session.setAttribute("result",result);
-
+        model.addAttribute("result",result);
         System.out.println("add result! "+ result);
-        return  "result.jsp";
+        return  "result";
     }
 //    @RequestMapping("add")
 //    public String add(HttpServletRequest req, HttpSession session){
